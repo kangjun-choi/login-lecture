3b86d6fc8ea9992ad67c2f3c64c223da360d9818
최강준 <zopqdkak@naver.com>
2022-05-09 19:49:25 +0900

    프론트단 javascript 파일 만들기

--- app/app.js
+++ app/app.js
@@ -15,5 +15,9 @@ app.set("view engine", "ejs"); // view 엔진 설정
 
 app.use("/", home);  // use => 미들 웨어를 등록해주는 메서드.
 
+// express.static : 정적 경로 추가
+// __dirname : 현재 경로를 표시(app.js 경로를 표시)
+app.use(express.static(`${__dirname}/src/public`));
+
 
 module.exports = app;
